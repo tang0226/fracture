ae692bb43e7e7380ba5ccf2e44d3e93e2f6e086f
tang0226 <58008827+tang0226@users.noreply.github.com>
2022-10-29 11:42:55 -0700

    Remove redundant Complex.prototype functions

--- complex.js
+++ complex.js
@@ -13,29 +13,14 @@ var Complex = function(re, im) {
 	};
 	this.div = function(c) {
 		return new Complex(
-			(this.re * c.re + this.im * c.im) / (c.re * c.re + c.im * c.im),
-			(this.im * c.re - this.re * c.im) / (c.re * c.re + c.im * c.im)
+			(this.re * c.re + this.im * c.im) / (c.re ** 2 + c.im ** 2),
+			(this.im * c.re - this.re * c.im) / (c.re ** 2 + c.im ** 2)
 		);
 	};
+	this.exp = function(e) {
+		if(e == 1){
+			return this;
+		}
+		return this.mul(this.exp(e - 1));
+	};
 }
-Complex.prototype.add = function(c) {
-	return new Complex(this.re + c.re, this.im + c.im);
-};
-Complex.prototype.sub = function(c) {
-	return new Complex(this.re - c.re, this.im - c.im);
-};
-Complex.prototype.mul = function(c) {
-	return new Complex(this.re * c.re - this.im * c.im, this.re * c.im + this.im * c.re);
-};
-Complex.prototype.div = function(c) {
-	return new Complex(
-		(this.re * c.re + this.im * c.im) / (c.re ** 2 + c.im ** 2),
-		(this.im * c.re - this.re * c.im) / (c.re ** 2 + c.im ** 2)
-	);
-};
-Complex.prototype.exp = function(e) {
-	if(e == 1){
-		return this;
-	}
-	return this.mul(this.exp(e - 1));
-};
