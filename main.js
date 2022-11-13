c72610bddad32018abbf00ffdeadbab4433376ee
tang0226 <58008827+tang0226@users.noreply.github.com>
2022-11-12 21:37:42 -0800

    Reconsolidate all code into main.js

--- main.js
+++ main.js
@@ -8,6 +8,283 @@ noStroke();
 colorMode("HSL");
 
 
+// Complex number prototype
+var Complex = function(re, im) {
+	this.re = re;
+	this.im = im;
+	
+	this.add = function(c) {
+		return new Complex(this.re + c.re, this.im + c.im);
+	};
+
+	this.sub = function(c) {
+		return new Complex(this.re - c.re, this.im - c.im);
+	};
+
+	this.mul = function(c) {
+		return new Complex(this.re * c.re - this.im * c.im, this.re * c.im + this.im * c.re);
+	};
+
+	this.div = function(c) {
+		return new Complex(
+			(this.re * c.re + this.im * c.im) / (c.re ** 2 + c.im ** 2),
+			(this.im * c.re - this.re * c.im) / (c.re ** 2 + c.im ** 2)
+		);
+	};
+
+	this.exp = function(e) {
+		if(e == 1){
+			return this;
+		}
+		return this.mul(this.exp(e - 1));
+	};
+	
+	this.abs = function() {
+		return hypot(this.re, this.im);
+	};
+}
+
+
+
+/******************************
+FRACTAL PROTOTYPES: THEORETICAL MATHEMATICAL SETS IN THE COMPLEX PLANE
+******************************/ 
+{
+	var Julia = function(c) {
+		this.c = c;
+		
+		this.f = function(z) {
+			return z.mul(z).add(this.c);
+		};
+		
+		this.iterate = function(Z, iterations, smoothColoring) {
+			let z = Z;
+			let n = 0;
+			z = this.f(z);
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(2);
+			}
+			return n;
+		};
+	};
+
+	var MultiJulia = function(c, e) {
+		this.c = c;
+		this.e = e;
+		
+		this.f = function(z) {
+			return z.exp(e).add(this.c);
+		};
+		
+		this.iterate = function(Z, iterations, smoothColoring) {
+			let z = Z;
+			let n = 0;
+			z = this.f(z);
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(this.e);
+			}
+			return n;
+		};
+	};
+
+	var Mandelbrot = function() {
+		this.f = function(z, c) {
+			return z.exp(2).add(c);
+		};
+		
+		this.iterate = function(c, iterations, smoothColoring) {
+			let z = new Complex(0, 0);
+			let n = 0;
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z, c);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(2);
+			}
+			return n;
+		};
+	};
+
+	var Multibrot = function(e) {
+		this.e = e;
+		
+		this.f = function(z, c) {
+			return z.exp(this.e).add(c);
+		};
+		
+		this.iterate = function(c, iterations, smoothColoring) {
+			let z = new Complex(0, 0);
+			let n = 0;
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z, c);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(this.e);
+			}
+			return n;
+		};
+	};
+
+	var BurningShip = function() {
+		this.f = function(z, c) {
+			return (new Complex(abs(z.re), abs(z.im))).exp(2).add(c);
+		};
+
+		this.iterate = function(c, iterations, smoothColoring) {
+			let z = new Complex(0, 0);
+			let n = 0;
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z, c);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(2);
+			}
+			return n;
+		};
+	};
+
+	var MultiShip = function(e) {
+		this.e = e;
+		
+		this.f = function(z, c) {
+			return (new Complex(abs(z.re), abs(z.im))).exp(this.e).add(c);
+		};
+
+		this.iterate = function(c, iterations, smoothColoring) {
+			let z = new Complex(0, 0);
+			let n = 0;
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z, c);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(this.e);
+			}
+			return n;
+		};
+	};
+
+	var BSJulia = function(c) {
+		this.c = c;
+		
+		this.f = function(z) {
+			return (new Complex(abs(z.re), abs(z.im))).exp(2).add(this.c);
+		};
+		
+		this.iterate = function(Z, iterations, smoothColoring) {
+			let z = Z;
+			let n = 0;
+			z = this.f(z);
+			while(z.abs() <= 2 && n < iterations) {
+				z = this.f(z);
+				n++;
+			}
+			if(n < iterations && smoothColoring) {
+				return n + 1 - log(log(z.abs())) / log(2);
+			}
+			return n;
+		};
+	};
+}
+
+
+
+/******************************
+FRAME PROTOTYPE: REGION ON THE COMPLEX PLANE
+******************************/
+var Frame = function(center, reWidth, imHeight) {
+	this.center = center;
+	this.reWidth = reWidth;
+	this.imHeight = imHeight;
+	this.reMin = center.re - reWidth / 2;
+	this.reMax = center.re + reWidth / 2;
+	this.imMin = center.im - imHeight /2;
+	this.imMax = center.im + imHeight / 2;
+
+	this.toComplexCoords = function(x, y){
+		return {
+			re: this.reMin + (this.reWidth * x / WIDTH),
+			im: this.imMin + (this.imHeight * y / HEIGHT)
+		};
+	};
+};
+
+
+
+/******************************
+IMAGE PROTOTYPE: RENDERING OF A FRACTAL WITH ITERATIONS, REGION, AND CANVAS SIZE
+******************************/
+var Image = function(fractal, iterations, smoothColoring, frame) {
+	this.fractal = fractal;
+	this.iterations = iterations;
+	this.smoothColoring = smoothColoring;
+
+	this.frame = frame;
+	
+	this.reIter = frame.reWidth / WIDTH;
+	this.imIter = frame.imHeight / HEIGHT;
+	
+	this.currY = 0;
+	this.currIm = frame.imMin;
+	
+	this.drawing = true;
+	this.startTime = null;
+	this.renderTime = null;
+	
+	this.drawLayer = function() {
+		let currRe = this.frame.reMin;
+		for(let currX = 0; currX < WIDTH; currX++) {
+			let complex = new Complex(currRe, this.currIm);
+			let val = this.fractal.iterate(complex, this.iterations, this.smoothColoring);
+			
+			if(val == this.iterations) {
+				fill(0, 0, 0);
+			}
+			else {
+				// Color pallettes; need to generalize
+				//var col = scale(val, 0, this.iterations, 0, 255)
+				// BW: fill(scale(val, 0, this.iterations, 0, 255));
+				fill(scale(val, 0, this.iterations, 0, 360), 100, scale(val, 0, this.iterations, 0, 100));
+				// HSL: fill(scale(val, 0, this.iterations, 0, 360), 100, 50);
+				// fill(scale(val, 0, this.iterations, 0, 255), scale(val, 0, this.iterations, 0, 255), scale(val, 0, this.iterations, 0, 255));
+			}
+			
+			rect(currX, this.currY, 1, 1);
+			
+			currRe += this.reIter;
+		};
+		
+		this.currY += 1;
+		this.currIm += this.imIter;
+		
+		if(this.currY > WIDTH) {
+			this.drawing = false;
+		}
+		this.renderTime = new Date(new Date() - this.startTime);
+		toolbar.displayRenderTime(this.renderTime);
+	};
+	
+	this.reset = function() {
+		this.currY = 0;
+		this.currIm = this.frame.imMin;
+		this.drawing = true;
+		this.startTime = new Date();
+	};
+};
+
+
+
 // Fractals
 var julia1 = new Julia(new Complex(-0.778, -0.116));
 var julia2 = new Julia(new Complex(0.28, 0.008));
