06448479c004e4137f46141d97b8f1ff634a5e8d
tang0226 <58008827+tang0226@users.noreply.github.com>
2024-02-02 13:29:42 -0800

    Change Complex to use arrays

--- src/js/complex.js
+++ src/js/complex.js
@@ -5,60 +5,57 @@ COMPLEX NUMBERS
 // No prototype, too tedious and unnecessary
 // to type "new Complex()" each time
 function Complex(re, im) {
-    return {
-        re: re,
-        im: im
-    };
+    return [re, im];
 };
 
 // Object -> string "a+bi"
 Complex.toString = function(c) {
-    let string = c.re.toString();
-    if(c.im >= 0) {
+    let string = c[0].toString();
+    if(c[1] >= 0) {
         string += "+";
     }
-    return string + c.im.toString() + "i";
+    return string + c[1].toString() + "i";
 };
 
 // String "a+bi" -> object
 Complex.parseString = function(s) {
     let match = s.match(/^(-?\d+(\.\d+)?)((\+|-)(\d+(\.\d+)?))i$/);
     if(match) {
-        return Complex(Number(match[1]), Number(match[3]));
+        return [Number(match[1]), Number(match[3])];
     }
 };
 
 // Operators
-Complex.equals = function(c1, c2) {
-    return c1.re == c2.re && c1.im == c2.im;
+Complex.isEqual = function(c1, c2) {
+    return c1[0] == c2[0] && c1[1] == c2[1];
 };
 
 Complex.add = function(c1, c2) {
-    return Complex(c1.re + c2.re, c1.im + c2.im);
+    return Complex(c1[0] + c2[0], c1[1] + c2[1]);
 };
 
 Complex.sub = function(c1, c2) {
-    return Complex(c1.re - c2.re, c1.im - c2.im);
+    return Complex(c1[0] - c2[0], c1[1] - c2[1]);
 };
 
 Complex.mul = function(c1, c2) {
     return Complex(
-        c1.re * c2.re - c1.im * c2.im,
-        c1.re * c2.im + c1.im * c2.re
+        c1[0] * c2[0] - c1[1] * c2[1],
+        c1[0] * c2[1] + c1[1] * c2[0]
     );
 };
 
 Complex.div = function(c1, c2) {
     return Complex(
-        (c1.re * c2.re + c1.im * c2.im) / (c2.re * c2.re + c2.im * c2.im),
-        (c1.im * c2.re - c1.re * c2.im) / (c2.re * c2.re + c2.im * c2.im)
+        (c1[0] * c2[0] + c1[1] * c2[1]) / (c2[0] * c2[0] + c2[1] * c2[1]),
+        (c1[1] * c2[0] - c1[0] * c2[1]) / (c2[0] * c2[0] + c2[1] * c2[1])
     );
 };
 
 Complex.sq = function(c) {
     return Complex(
-        c.re ** 2 - c.im ** 2,
-        2 * c.re * c.im
+        c[0] ** 2 - c[1] ** 2,
+        2 * c[0] * c[1]
     );
 };
 
@@ -66,7 +63,7 @@ Complex.sq = function(c) {
 Complex.exp = function(c, e) {
     let r = c;
     let currE = 1;
-    while(currE < e) {
+    while (currE < e) {
         r = Complex.mul(r, c);
         currE++;
     }
@@ -76,10 +73,10 @@ Complex.exp = function(c, e) {
 // The absolute value of a complex number is its
 // Euclidean distance from the origin
 Complex.abs = function(c) {
-    return Math.sqrt(c.re ** 2 + c.im ** 2);
+    return Math.sqrt(c[0] ** 2 + c[1] ** 2);
 };
 
 // The complex conjugate a+bi is a-bi
 Complex.conj = function(c) {
-    return Complex(c.re, -c.im);
+    return Complex(c[0], -c[1]);
 }
